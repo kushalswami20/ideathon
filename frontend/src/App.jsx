06c092f73b645ad697cd1915a5f94f55b571cae7
rahul071845 <machrarahul25@gmail.com>
2025-02-02 09:48:24 +0530

    Fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,30 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/Users/kushalarogyaswami/Documents/ideathon-1/frontend/src/assets/Logo.jpeg'
-//dont use app.css
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Footer from './components/shared/Footer'
-import Navbar from './components/shared/Navbar'
-import Home from './components/Home'
-import Contact from './components/Contact'
-import Resources from './components/Resources'
-import Team from './components/Team'
-import Gallery from './components/Gallery'
-
-import Registration from './components/Registration'
-import ScrollToTop from './components/ScrollToTop';
-
+import Footer from './components/shared/Footer';
+import Navbar from './components/shared/Navbar';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Resources from './components/Resources';
+import Team from './components/Team';
+import Gallery from './components/Gallery';
+import Registration from './components/Registration';
+import ScrollToTop from './components/scrollToTop';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Router>
+      <ScrollToTop /> {/* Add this to handle scrolling on navigation */}
       <Navbar />
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/Registration" element= {<Registration />} />
-      <Route path="/contact" element= {<Contact />} />
-      <Route path="/resources" element= {<Resources />} />
-      <Route path="/gallery" element= {<Gallery />} />
-      <Route path="/team" element= {<Team />} />
-
+        <Route path="/" element={<Home />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/team" element={<Team />} />
       </Routes>
       <Footer />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
